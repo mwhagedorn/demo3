35808a5ae916bbf111bc094172d1fd78c534a15a
Mike Hagedorn <mike@silverchairsolutions.com>
2011-08-08 06:49:00 -0500

    ffcrm bootstrap done

--- cookbooks/bootstrap_rake_tasks/recipes/default.rb
+++ cookbooks/bootstrap_rake_tasks/recipes/default.rb
@@ -1,14 +1,19 @@
+
+package "rake" do
+  action :install
+end
+
 bash "create FFCRM database" do
   cwd File.join(node[:bundler][:apps_path], node[:bundler][:app])
-  code "rake db:create RAILS_ENV=production"
+  code "bundle exec rake db:create RAILS_ENV=production"
 end
 
 bash "bootstrap CRM data schema" do
   cwd File.join(node[:bundler][:apps_path], node[:bundler][:app])
-  code "rake crm:setup RAILS_ENV=production"
+  code "bundle exec rake crm:setup RAILS_ENV=production"
 end
 
 bash "bootstrap CRM data" do
   cwd File.join(node[:bundler][:apps_path], node[:bundler][:app])
-  code "rake crm:load RAILS_ENV=production"
+  code "bundle exec rake crm:demo:load RAILS_ENV=production"
 end
