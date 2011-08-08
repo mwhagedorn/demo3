89347535ccdc7b444c95b159f15e3f4961a66791
Mike Hagedorn <mike@silverchairsolutions.com>
2011-08-07 22:11:26 -0500

    bundle complete

--- cookbooks/vagrant_main/recipes/default.rb
+++ cookbooks/vagrant_main/recipes/default.rb
@@ -1,6 +1,13 @@
 package "mysql-server" do
  action :install
 end
+package "libmysqlclient16-dev" do
+  action :install
+end
 
 require_recipe "copy_database_yml"
+gem_package "bundler" do
+  action :install
+end
+require_recipe "git"
 require_recipe "bundler::install"
